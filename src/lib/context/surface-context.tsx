8db823f6938f2008781b04969fca36472211f824
Enijar <TheEnijar@gmail.com>
2022-08-21 11:03:41 +0100

    chore: remove old files

--- src/lib/context/surface-context.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const SurfaceContext = React.createContext<any>({});
--- src/lib/state/surfaces.ts
+++
@@ -1,9 +0,0 @@
-type Surface = {
-  id: string;
-  width: number;
-  height: number;
-};
-
-const surfaces: { [parentId: string]: Surface[] } = {};
-
-export default surfaces;
