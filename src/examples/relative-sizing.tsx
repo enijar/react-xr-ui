2352be7ec811be50f066a1dcffb5def21f4c477b
Enijar <TheEnijar@gmail.com>
2022-09-22 22:25:04 +0100

    chore: add buttons to relative sizing example

--- src/examples/relative-sizing.tsx
+++ src/examples/relative-sizing.tsx
@@ -2,13 +2,54 @@ import React from "react";
 import Example from "@/components/example";
 import { Layer } from "../../lib";
 import ViewCode from "@/components/view-code";
+import Button from "@/components/button";
 
 export default function RelativeSizing() {
+  const sizes = React.useMemo(() => {
+    return {
+      xl: "75%",
+      m: "50%",
+      s: "25%",
+    };
+  }, []);
+
+  const [size, setSize] = React.useState<keyof typeof sizes>("m");
+
   return (
     <>
       <Example>
+        <Layer width={1} height={0.1} gap={0.05} position-y={0.6}>
+          <Button
+            width={0.1}
+            height={0.1}
+            fontSize={0.35}
+            textContent={sizes.xl}
+            selected={size === "xl"}
+            onClick={() => setSize("xl")}
+          />
+          <Button
+            width={0.1}
+            height={0.1}
+            fontSize={0.35}
+            textContent={sizes.m}
+            selected={size === "m"}
+            onClick={() => setSize("m")}
+          />
+          <Button
+            width={0.1}
+            height={0.1}
+            fontSize={0.35}
+            textContent={sizes.s}
+            selected={size === "s"}
+            onClick={() => setSize("s")}
+          />
+        </Layer>
         <Layer gap={0.1} backgroundColor="crimson">
-          <Layer width="50%" height="50%" backgroundColor="#333333" />
+          <Layer
+            width={sizes[size]}
+            height={sizes[size]}
+            backgroundColor="#333333"
+          />
         </Layer>
       </Example>
       <ViewCode pathname="relative-sizing" />
